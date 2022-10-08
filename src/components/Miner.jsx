c50aaefc35f8ae19910d7f5b60b7aaa08dd8997e
vinyl704 <kingvinyl@gmail.com>
2022-10-07 21:24:55 -0400

    timeframe state update problem patched

--- src/components/Miner.jsx
+++ src/components/Miner.jsx
@@ -42,16 +42,16 @@ export default function Miner(props){
         
     return (
     
-        <div className="card mx-auto mt-2 mb-3 w-35 bg-primary text-white text-center" onClick={handleClick} >
+        <div className="card mx-auto mt-2 mb-3 w-35 bg-primary text-white text-center">
             <h4 className="card-header text-outline">{name}</h4>
             <div className="card-body d-flex flex-column text-center justify-content-center align-items-center">
             <p className="card-item d-flex flex-column flex-wrap text-center text-break">Miner Address: {address}</p>
             <p className="card-item">Distance: {isMeters?distance.toFixed(2):metersToMile(distance).toFixed(2)} {unit}</p>
             <p className="card-item">{geocode.short_street}</p>
             <p className="card-item">{geocode.short_city}, {geocode.short_state}</p>
-            <div className="card-item mb-2"><Rewards {...props} rewards={rewards} timeFrame={timeFrame} clicked={clicked}/></div>
+            <div className="card-item mb-2" onClick={handleClick} ><Rewards {...props} rewards={rewards} timeFrame={timeFrame} clicked={clicked}/></div>
             <p className={status.online==="offline"?"card-item bg-danger rounded p-1":"card-item rounded p-1 bg-success"}>{status.online}</p>
-            <TimeFrame setTimeFrame={setTimeFrame} timeFrame={timeFrame} />
+            <TimeFrame data={fetchData} setTimeFrame={setTimeFrame} timeFrame={timeFrame} />
 
             </div>
         </div>
--- src/components/TimeFrame.jsx
+++ src/components/TimeFrame.jsx
@@ -1,11 +1,14 @@
+import { useEffect } from "react"
 
-export default function TimeFrame({setTimeFrame,timeFrame}){
+export default function TimeFrame({data,setTimeFrame,timeFrame}){
     const handleChange=(e)=>{
         e.preventDefault()
         e.stopPropagation()
 
-        setTimeFrame(e.target.value)
+        setTimeFrame(timeFrame)
     }
+
+    useEffect(()=>data,[timeFrame])
     return(
         <select className="form-control-lg" onChange={handleChange} value={timeFrame}>
             <option value="1">1 Day</option>
